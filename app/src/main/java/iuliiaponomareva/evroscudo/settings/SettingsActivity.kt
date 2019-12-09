48ffab51666f1fa7443d667f5168620bf62b04de
Iuliia <iuliiaponomareva@gmail.com>
2019-12-09 16:20:30 +0300

    Simplify settings code, display action bar

--- app/src/main/java/iuliiaponomareva/evroscudo/settings/SettingsActivity.kt
+++ app/src/main/java/iuliiaponomareva/evroscudo/settings/SettingsActivity.kt
@@ -1,47 +1,26 @@
 package iuliiaponomareva.evroscudo.settings
 
 import android.os.Bundle
-import android.preference.PreferenceActivity
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.CheckBoxPreference
-import androidx.preference.PreferenceFragment
+import androidx.preference.PreferenceFragmentCompat
 import iuliiaponomareva.evroscudo.R
 
 
-/**
- * A [PreferenceActivity] that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- *
- * See [Android Design: Settings](http://developer.android.com/design/patterns/settings.html)
- * for design guidelines and the [Settings API Guide](http://developer.android.com/guide/topics/ui/settings.html)
- * for more information on developing a Settings UI.
- */
-class SettingsActivity : AppCompatPreferenceActivity() {
+class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupActionBar()
-        // load settings fragment
-        fragmentManager.beginTransaction().replace(android.R.id.content, GeneralPreferenceFragment())
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
             .commit()
-    }
-
-    private fun setupActionBar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onMenuItemSelected(featureId: Int, item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == android.R.id.home) {
-           onBackPressed()
-        }
-        return super.onMenuItemSelected(featureId, item)
-    }
 
-
-    class GeneralPreferenceFragment : PreferenceFragment() {
+    class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setHasOptionsMenu(true)
             val screen = preferenceManager.createPreferenceScreen(activity)
